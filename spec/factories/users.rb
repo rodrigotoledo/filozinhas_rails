0ca2b20a7cdcd10a81e1fd1c613db59735612dd1
rodrigo.toledo <rodrigo.toledo@creditcorp.com.br>
2022-07-03 13:21:23 -0300

    factory of user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    email { 'user@email.com.br' }
+    password { 'asdqwe123' }
+    password_confirmation { 'asdqwe123' }
+    trait :admin do
+      email { 'admin@email.com.br' }
+    end
   end
 end
